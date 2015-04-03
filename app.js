b1b800337252b804509267df64992e4b654caf02
anthonyvia <anthv123@gmail.com>
2015-04-03 11:08:44 -0700

    Initial app structure.

---
+++ app.js
@@ -0,0 +1,51 @@
+require('node-jsx').install({extension:'.jsx'});
+var express = require('express');
+var path = require('path');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var bowlScoreApi = require('./lib/server/api');
+var config = require('./lib/config');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// logs each request, response code, and response time
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', bowlScoreApi);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
---
+++ lib/app.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <span>Bowl Score</span>
+    )
+  }
+});
+
+React.render(
+  <App />,
+  document.body
+);
