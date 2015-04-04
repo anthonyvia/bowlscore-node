f663803451d59ae1c040e6f1388d8471d58041d0
anthonyvia <anthv123@gmail.com>
2015-04-03 17:27:47 -0700

    Handle basic updating of frames.

--- lib/app.jsx
+++ lib/app.jsx
@@ -9,20 +9,35 @@ var App = React.createClass({
     )
   },
 
+  updateTotal: function () {
+    var total = this.refs.frameOne.state.ballOne + this.refs.frameOne.state.ballTwo +
+      this.refs.frameTwo.state.ballOne + this.refs.frameTwo.state.ballTwo +
+      this.refs.frameThree.state.ballOne + this.refs.frameThree.state.ballTwo +
+      this.refs.frameFour.state.ballOne + this.refs.frameFour.state.ballTwo +
+      this.refs.frameFive.state.ballOne + this.refs.frameFive.state.ballTwo +
+      this.refs.frameSix.state.ballOne + this.refs.frameSix.state.ballTwo +
+      this.refs.frameSeven.state.ballOne + this.refs.frameSeven.state.ballTwo +
+      this.refs.frameEight.state.ballOne + this.refs.frameEight.state.ballTwo +
+      this.refs.frameNine.state.ballOne + this.refs.frameNine.state.ballTwo +
+      this.refs.frameTen.state.ballOne + this.refs.frameTen.state.ballTwo + this.refs.frameTen.state.ballThree;
+
+    this.setState({ total: total });
+  },
+
   render: function () {
     return (
       <div>
         <span>Bowl Score</span>
-        <Frame frameNumber="1"/>
-        <Frame frameNumber="2"/>
-        <Frame frameNumber="3"/>
-        <Frame frameNumber="4"/>
-        <Frame frameNumber="5"/>
-        <Frame frameNumber="6"/>
-        <Frame frameNumber="7"/>
-        <Frame frameNumber="8"/>
-        <Frame frameNumber="9"/>
-        <Frame frameNumber="10"/>
+        <Frame frameNumber="1" ref="frameOne" updateTotal={this.updateTotal} />
+        <Frame frameNumber="2" ref="frameTwo" updateTotal={this.updateTotal} />
+        <Frame frameNumber="3" ref="frameThree" updateTotal={this.updateTotal} />
+        <Frame frameNumber="4" ref="frameFour" updateTotal={this.updateTotal} />
+        <Frame frameNumber="5" ref="frameFive" updateTotal={this.updateTotal} />
+        <Frame frameNumber="6" ref="frameSix" updateTotal={this.updateTotal} />
+        <Frame frameNumber="7" ref="frameSeven" updateTotal={this.updateTotal} />
+        <Frame frameNumber="8" ref="frameEight" updateTotal={this.updateTotal} />
+        <Frame frameNumber="9" ref="frameNine" updateTotal={this.updateTotal} />
+        <Frame frameNumber="10" ref="frameTen" updateTotal={this.updateTotal} />
         <Total total={this.state.total} />
       </div>
     )
--- lib/components/frame.jsx
+++ lib/components/frame.jsx
@@ -11,12 +11,54 @@ var Frame = React.createClass({
     )
   },
 
+  getBallValue: function (ball) {
+    var value = React.findDOMNode(ball).value;
+
+    if (value === undefined || value === "") {
+      value = 0;
+    }
+
+    return value;
+  },
+
+  handleChange: function (ballNum) {
+    if (ballNum === undefined) {
+      return;
+    }
+
+    if (ballNum === "1") {
+      this.setState({ ballOne: this.getBallValue(this.refs.ballOne) });
+    } else if (ballNum === "2") {
+      this.setState({ ballTwo: this.getBallValue(this.refs.ballTwo) });
+    } else if (ballNum === "3") {
+      this.setState({ ballThree: this.getBallValue(this.refs.ballThree )});
+    }
+  },
+
+  /*
+   * Required to avoid an infinite loop. On a ball change, componentDidUpdate eventually gets called,
+   * which eventually changes the state of the parent component (total score),
+   * which then causes an update of the parent's elements, which then triggers
+   * componentDidUpdate eventually again.
+   */
+  shouldComponentUpdate: function (nextProps, nextState) {
+    if (this.state.ballOne !== nextState.ballOne || this.state.ballTwo !== nextState.ballTwo || this.state.ballThree !== nextState.ballThree) {
+      return true;
+    }
+
+    return false;
+  },
+
+  componentDidUpdate: function () {
+    this.props.updateTotal();
+  },
+
   render: function () {
     return (
       <div>
         <span>{this.props.frameNumber}</span>
-        <input className="frame-input" type="text" maxLength="2" defaultValue={this.state.ballOne} />
-        <input className="frame-input" type="text" maxLength="2" defaultValue={this.state.ballTwo} />
+        <input className="frame-input" type="text" maxLength="2" defaultValue={this.state.ballOne} ref="ballOne" onChange={this.handleChange.bind(null, "1")}/>
+        <input className="frame-input" type="text" maxLength="2" defaultValue={this.state.ballTwo} ref="ballTwo" onChange={this.handleChange.bind(null, "2")}/>
       </div>
     );
   }
